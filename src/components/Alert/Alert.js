735e7e876955616fb0d7e64a7b27c1c263cf1c45
WannabeRockStar <30549855+WannabeRockStar@users.noreply.github.com>
2022-09-01 21:59:31 +0400

    hands icon

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,7 +1,7 @@
 const Alert = () => {
     return (
         <div className="bg-gradient-to-r from-white to-[#E9F0FF] px-5 py-3 shadow-alert rounded-lg">
-            <h2 className="text-[#953849] text-[30px] font-firagoBold leading-10">საღამო მშვიდობისა, მაკა</h2>
+            <h2 className="text-[#953849] text-[30px] font-firagoBold leading-10">საღამო მშვიდობისა, მაკა 👋</h2>
             <p className="leading-6 text-[#333333] mt-2">შენი დღის განრიგი ასეთია</p>
         </div>
     );
